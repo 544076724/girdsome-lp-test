6b43433067b695192f343079acbcd76956babede
luopeng <luopeng@viewhigh.com>
2021-01-17 18:55:19 +0800

    1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,14 @@
 <template>
   <Layout>
 
-    <div class="container"  :style="{marginTop:'6rem'}">
+    <div class="container" :style="{marginTop:'6rem'}">
       <div class="hero">
         <h1 class="hero-title">简单。美学。值。</h1>
         <h2 class="hero-subtitle">嗨，我是一名程序员，这是我的一个静态站点测试demo。</h2>
       </div>
       <div data-v-50cbff3e="" class="projects">
         <div v-for="topPost in $page.topPosts.edges" :key="topPost.node.id" data-v-50cbff3e="" class="project">
-          <g-link :to="topPost.node.path" data-v-50cbff3e=""  class="project-link">
+          <g-link :to="topPost.node.path" data-v-50cbff3e="" class="project-link">
             <img :style="{width:'100%'}" :src="GRIDSOME_API_URL+topPost.node.cover.url" alt="" data-v-50cbff3e="" class="thumbnail g-image g-image--lazy g-image--loaded">
             <h3 data-v-50cbff3e="" class="project-title">{{topPost.node.title}}</h3>
             <div data-v-50cbff3e="" class="categories" :style="{width:'6rem',height:'6rem',backgroundImage:`url(${GRIDSOME_API_URL+topPost.node.cover.url})`}">
@@ -21,29 +21,22 @@
       </div>
     </div>
     <div data-v-460714ac="">
-      <div 
-      
-      data-v-460714ac="" class="latest-journals-heading container"><span data-v-460714ac="" class="label">
+      <div data-v-460714ac="" class="latest-journals-heading container"><span data-v-460714ac="" class="label">
           <font style="vertical-align: inherit;">
             <font style="vertical-align: inherit;">最新和最热的</font>
           </font>
         </span></div>
       <div data-v-460714ac="" class="latest-journals">
         <div data-v-460714ac="" class="container">
-          <g-link
-          v-for="topPost in $page.noTopPosts.edges"
-          :key="topPost.node.id"
-          :to="topPost.node.path"
-           data-v-460714ac=""  class="journal">
+          <g-link v-for="topPost in $page.noTopPosts.edges" :key="topPost.node.id" :to="topPost.node.path" data-v-460714ac="" class="journal">
             <h3 data-v-460714ac="" class="journal-title">
               <font style="vertical-align: inherit;">
                 <font style="vertical-align: inherit;">{{topPost.node.title}}</font>
               </font>
             </h3>
           </g-link>
-          
-          
-          </div>
+
+        </div>
       </div>
     </div>
   </Layout>
@@ -81,7 +74,11 @@ query{
 export default {
   name: 'IndexPage',
   metaInfo: {
-    title: '首页'
+    title: '首页',
+    meta: [{                 // set meta
+      name: 'referrer',
+        content: 'origin'
+    }]
   }
 }
 </script>
